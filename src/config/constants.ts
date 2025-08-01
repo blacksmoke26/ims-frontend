5e31305892e740be1cfc6628c1ab3fcd1ecb223a
Junaid Atari <mj.atari@gmail.com>
2025-08-01 21:26:37 +0500

    feat: create constants file

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Repository: https://github.com/blacksmoke26/ims-frontend
+
+
+export const AUTH_TOKEN_KEY = '$AUTH_TOKEN$';
